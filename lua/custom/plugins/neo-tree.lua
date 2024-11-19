c7498dea09f2751d0ad607b10b1da6570b1c27cf
Hector Lopez <hlopezcarballo@gmail.com>
2024-11-19 09:31:34 -0600

    add neotree config

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -4,6 +4,25 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
   keys = {
-    { '<leader>e', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    -- { '<leader>e', ':Neotree reveal current<CR>', desc = 'NeoTree reveal', silent = true },
+    { '\\', ':Neotree reveal current<CR>', desc = 'NeoTree reveal current', silent = true },
+  },
+  opts = {
+    event_handlers = {
+      {
+        event = 'file_opened',
+        handler = function()
+          require('neo-tree.command').execute { action = 'close' }
+        end,
+      },
+    },
+    filesystem = {
+      hijack_netrw_behavior = 'open_current',
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
   },
 }
