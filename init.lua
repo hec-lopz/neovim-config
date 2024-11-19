b5c088d26e6775c4f584f7d7b912faa595073a07
Hector Lopez <hlopezcarballo@gmail.com>
2024-11-19 09:31:34 -0600

    add neotree config

--- init.lua
+++ init.lua
@@ -319,7 +319,7 @@ require('lazy').setup({
       spec = {
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>G', group = '[G]it Hunk', mode = { 'n', 'v' } },
         { '<leader>l', group = '[L]SP', mode = { 'n', 'v' } },
       },
     },
@@ -706,6 +706,28 @@ require('lazy').setup({
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
+    config = function(_, opts)
+      require('conform').setup(opts)
+
+      vim.api.nvim_create_user_command('FormatDisable', function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = 'Disable autoformat-on-save',
+        bang = true,
+      })
+
+      vim.api.nvim_create_user_command('FormatEnable', function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = 'Re-enable autoformat-on-save',
+      })
+    end,
   },
 
   { -- Autocompletion
